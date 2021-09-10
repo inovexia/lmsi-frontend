ea23461df2765ab3115aef8fb7a1a2e4f82ff1c1
Roshan kumar <roshan31kumar31@gmail.com>
2021-09-10 17:05:57 +0530

    complete login and  register page

--- src/views/auth/sign-in/defaultView.js
+++ src/views/auth/sign-in/defaultView.js
@@ -1,74 +1,140 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import { Button } from 'src/components/Buttons'
 import { Icon, GoogleIcon } from 'src/components/Icon'
 
 const SignIn = () => {
+  const [show, setShow] = useState(false)
+
+  const handleSubmit = () => {
+    setShow(true)
+  }
+
+  const handleChange = () => {
+    setShow(false)
+  }
+
   return (
-    <section className={'signIn-card'}>
-      <div className={'card-heading'}>
-        <h5>Log in to your account</h5>
+    <>
+      <div className={'Info-card'}>
+        <div className={'card-heading'}>
+          <h5>Log in to your account</h5>
+        </div>
+        <form action="">
+          <div className={'form-group'}>
+            <input
+              onFocus={handleChange}
+              className={'form-data'}
+              type="email"
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+          {show ? (
+            <div className={'form-group'}>
+              <input
+                className={'form-data'}
+                type="password"
+                placeholder="Enter your password"
+              />
+            </div>
+          ) : (
+            ''
+          )}
+          <div className={'form-group'}>
+            <Button
+              onClick={handleSubmit}
+              type="button"
+              variant="primary"
+              label={show ? 'Log in' : 'Continue'}
+            />
+          </div>
+        </form>
+        <h6 className={'text-center my-3'}>
+          <small>OR</small>
+        </h6>
+        <div className={'social-login'}>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <GoogleIcon className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With Google
+            </span>
+          </Button>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <Icon icon={'facebook'} className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With Facebook
+            </span>
+          </Button>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <Icon icon={'linkedin'} className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With LinkedIn
+            </span>
+          </Button>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <Icon icon={'twitter'} className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With Twitter
+            </span>
+          </Button>
+        </div>
+
+        <div className={'Info-card-footer'}>
+          <ul>
+            <li>
+              <NavLink to={'/auth/forgot-password'}>
+                <span>Can't log in?</span>
+              </NavLink>
+            </li>
+            <p style={{ margin: '0px 8px' }}>•</p>
+            <li>
+              <NavLink to={`/auth/sign-up`}>
+                <span>Register for an account</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-      <form action="">
-        <div className={'form-group'}>
-          <input className={'form-data'} type="email" />
+
+      <div className={'footer-top'}>
+        <div>
+          <ul>
+            <li>
+              <NavLink to={'#'}>
+                <span>Privacy Policy</span>
+              </NavLink>
+            </li>
+            <p style={{ margin: '0px 8px' }}>•</p>
+            <li>
+              <NavLink to={`#`}>
+                <span>User Notice</span>
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <div className={'form-group'}>
-          <input className={'form-data'} type="password" />
-        </div>
-        <div className={'form-group'}>
-          <Button type="submit" variant="primary" label="Log in" />
-        </div>
-      </form>
-      <h6 className={'text-center my-3'}>
-        <small>OR</small>
-      </h6>
-      <div className={'social-login'}>
-        <Button
-          className={'d-flex w-100 border'}
-          variant="light"
-          disabled={true}
-          title={'Coming Soon'}
-        >
-          <GoogleIcon className={'my-auto'} />
-          <span className={'flex-grow-1 text-center'}>
-            Continue With Google
-          </span>
-        </Button>
-        <Button
-          className={'d-flex w-100 border'}
-          variant="light"
-          disabled={true}
-          title={'Coming Soon'}
-        >
-          <Icon icon={'facebook'} className={'my-auto'} />
-          <span className={'flex-grow-1 text-center'}>
-            Continue With Facebook
-          </span>
-        </Button>
-        <Button
-          className={'d-flex w-100 border'}
-          variant="light"
-          disabled={true}
-          title={'Coming Soon'}
-        >
-          <Icon icon={'linkedin'} className={'my-auto'} />
-          <span className={'flex-grow-1 text-center'}>
-            Continue With LinkedIn
-          </span>
-        </Button>
-        <Button
-          className={'d-flex w-100 border'}
-          variant="light"
-          disabled={true}
-          title={'Coming Soon'}
-        >
-          <Icon icon={'twitter'} className={'my-auto'} />
-          <span className={'flex-grow-1 text-center'}>
-            Continue With Twitter
-          </span>
-        </Button>
       </div>
-    </section>
+    </>
   )
 }
 
--- src/views/auth/sign-up/defaultView.js
+++ src/views/auth/sign-up/defaultView.js
@@ -1,12 +1,128 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { ucFirst, getRoleId } from 'src/helpers/Utils'
+import { Button } from 'src/components/Buttons'
+import { Icon, GoogleIcon } from 'src/components/Icon'
+
 const SignUp = ({
   match: {
     params: { InstituteId, Method, MethodValue, RoleKey = 'learner' },
   },
 }) => {
   false && console.log(InstituteId, Method, MethodValue, getRoleId(RoleKey))
-  return <h1>{`Sign Up ${ucFirst(RoleKey)}`}</h1>
+  return (
+    <>
+      <div className={'Info-card'}>
+        <div className={'card-heading'}>
+          <h5>{`Register your account ${ucFirst(RoleKey)}`}</h5>
+        </div>
+        <form action="">
+          <div className={'form-group'}>
+            <input
+              className={'form-data'}
+              type="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className={'form-group'}>
+            <input
+              className={'form-data'}
+              type="text"
+              placeholder="Enter your full name"
+            />
+          </div>
+          <div className={'term-conditions'}>
+            <span>
+              By signing up, I accept the LMSI{' '}
+              <NavLink to={'#'}>
+                <span>Cloud Terms of Service</span>
+              </NavLink>{' '}
+              and acknowledge the{' '}
+              <NavLink to={'#'}>
+                <span>Privacy Policy</span>
+              </NavLink>
+            </span>
+          </div>
+          <div className={'form-group'}>
+            <Button type="submit" variant="primary" label="Register" />
+          </div>
+        </form>
+        <h6 className={'text-center my-3'}>
+          <small>OR</small>
+        </h6>
+        <div className={'social-login'}>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <GoogleIcon className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With Google
+            </span>
+          </Button>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <Icon icon={'facebook'} className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With Facebook
+            </span>
+          </Button>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <Icon icon={'linkedin'} className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With LinkedIn
+            </span>
+          </Button>
+          <Button
+            className={'d-flex w-100 border'}
+            variant="light"
+            disabled={true}
+            title={'Coming Soon'}
+          >
+            <Icon icon={'twitter'} className={'my-auto'} />
+            <span className={'flex-grow-1 text-center'}>
+              Continue With Twitter
+            </span>
+          </Button>
+        </div>
+
+        <div className={'Info-card-footer'}>
+          <ul>
+            <li>
+              <NavLink to={'/auth/sign-in'}>
+                <span>Already have an LMSI account? Log in</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={'footer-top'}>
+        <span>
+          This page is protected by reCAPTCHA and the Google{' '}
+          <NavLink to={'#'}>
+            <span>Privacy Policy</span>
+          </NavLink>{' '}
+          and{' '}
+          <NavLink to={'#'}>
+            <span>Terms of Service</span>
+          </NavLink>{' '}
+          apply
+        </span>
+      </div>
+    </>
+  )
 }
 
 export default SignUp
