f404f3261a138825e58425bff01eddafdd00bbe1
Roshan kumar <roshan31kumar31@gmail.com>
2021-09-11 17:24:49 +0530

    forgot or set  password page complete

--- src/views/auth/forgot-password/defaultView.js
+++ src/views/auth/forgot-password/defaultView.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Button } from 'src/components/Buttons'
 
 const ForgotPassword = () => {
   return (
-    <div>
-      <h1>Forgot Password</h1>
-    </div>
+    <>
+      <div className={'Info-card'}>
+        <div className={'card-heading'}>
+          <h5>Can't log in?</h5>
+        </div>
+        <form action="">
+          <label className={'input-label mt-4'} htmlFor="email">
+            We'll send a recovery link to
+          </label>
+          <div className={'form-group mt-0'}>
+            <input
+              className={'form-data'}
+              name="email"
+              id="email"
+              type="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+
+          <div className={'form-group'}>
+            <Button
+              type="submit"
+              variant="primary"
+              label="Send recovery link"
+            />
+          </div>
+        </form>
+
+        <div className={'Info-card-footer'}>
+          <ul>
+            <li>
+              <NavLink to={'/auth/sign-in'}>
+                <span>Return to log in</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={'footer-top'}>
+        <div>
+          <ul>
+            <li>
+              <NavLink to={'#'}>
+                <span>Login help</span>
+              </NavLink>
+            </li>
+            <p style={{ margin: '0px 8px' }}>•</p>
+            <li>
+              <NavLink to={`#`}>
+                <span>Contact Support</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/views/auth/set-password/defaultView.js
+++ src/views/auth/set-password/defaultView.js
@@ -1,10 +1,69 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Button } from 'src/components/Buttons'
 
 const SetPassword = () => {
   return (
-    <div>
-      <h1>Set Password</h1>
-    </div>
+    <>
+      <div className={'Info-card'}>
+        <div className={'card-heading'}>
+          <h5>Set your new password</h5>
+        </div>
+        <form action="">
+          <div className={'form-group'}>
+            <input
+              className={'form-data'}
+              name="password"
+              id="password"
+              type="password"
+              placeholder="Enter your new password"
+            />
+          </div>
+
+          <div className={'form-group'}>
+            <input
+              className={'form-data'}
+              name="password"
+              id="password"
+              type="password"
+              placeholder="Confirm your new password"
+            />
+          </div>
+
+          <div className={'form-group'}>
+            <Button type="submit" variant="primary" label="Change password" />
+          </div>
+        </form>
+
+        <div className={'Info-card-footer'}>
+          <ul>
+            <li>
+              <NavLink to={'/auth/sign-in'}>
+                <span>Return to log in</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={'footer-top'}>
+        <div>
+          <ul>
+            <li>
+              <NavLink to={'#'}>
+                <span>Login help</span>
+              </NavLink>
+            </li>
+            <p style={{ margin: '0px 8px' }}>•</p>
+            <li>
+              <NavLink to={`#`}>
+                <span>Contact Support</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
