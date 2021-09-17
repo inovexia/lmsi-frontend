fc2322ba5d9cdce6db728eca96fbc5cb720c6f35
Ankit Shukla <4nkit5hukla@gmail.com>
2021-09-17 09:46:32 +0530

    AppReducer Fixed.

--- src/AppContext/AppReducer/index.js
+++ src/AppContext/AppReducer/index.js
@@ -3,9 +3,8 @@ import * as AllActions from 'src/constants/actions'
 const AppReducer = (appStore, AppAction) => {
   switch (AppAction.type) {
     case AllActions.LOAD_USER:
-      return { ...appStore, user: AppAction.user }
+      return { ...appStore, isLoggedIn: true, user: AppAction.user }
     case AllActions.LOGIN_USER:
-      console.log(AppAction.payload)
       return { ...appStore, isLoggedIn: true, user: AppAction.payload.user }
     default:
       return appStore
