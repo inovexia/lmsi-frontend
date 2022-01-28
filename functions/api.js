c0c0f0ced53d6605f6c7fbd49b7e654b0aa04d98
Ankit Shukla <ankit5hukla@hotmail.com>
2022-01-29 00:58:18 +0530

    Api Updates

--- functions/api.js
+++ functions/api.js
@@ -1,26 +1,12 @@
-/* eslint-disable no-undef */
-// ? For absolute path enable import function instead of require
-const fs = require('fs')
-global.include = z => {
-  let x = __dirname.replace(/\\/g, '/')
-  let y = x.replace('/node_modules/node-absolute-path', '')
-  let ext = ['', '.ts', '.tsx', '.js', '.jsx', '.html', '.css', '.png', '.jpg']
-  for (let i = 0; i < ext.length; i++) {
-    if (fs.existsSync(`${y}/${z}${ext[i]}`)) {
-      z = `${y}/${z}`
-    }
-  }
-  return require(z)
-}
-
 // ? For accessing .env file variables
 require('dotenv').config()
 
 const express = require('express'),
   db = require('mongoose'),
   serverless = require('serverless-http'),
-  { apiRoute } = include('routes'),
-  app = express()
+  { Router } = express,
+  app = express(),
+  apiRoute = Router()
 
 db.connect(
   process.env.NODE_ENV === 'development'
@@ -32,24 +18,24 @@ db.connection.on('connected', () => console.log(`DB connected successfully`))
 
 db.connection.on('error', error => console.error(error.message))
 
-// apiRoute.get('/', (req, res) =>
-//   res.status(200).json({ message: 'Ping Successful API Server Running...' })
-// )
+apiRoute.get('/', (req, res) =>
+  res.status(200).json({ message: 'Ping Successful API Server Running...' })
+)
 
-// apiRoute.get('/users', (req, res) =>
-//   res.json({
-//     users: [
-//       {
-//         name: 'steve'
-//       },
-//       {
-//         name: 'joe'
-//       }
-//     ]
-//   })
-// )
+apiRoute.get('/users', (req, res) =>
+  res.json({
+    users: [
+      {
+        name: 'steve'
+      },
+      {
+        name: 'joe'
+      }
+    ]
+  })
+)
 
-// apiRoute.get('/hello', (req, res) => res.json({ msg: 'hello world' }))
+apiRoute.get('/hello', (req, res) => res.json({ msg: 'hello world' }))
 
 app.use([express.urlencoded({ extended: true }), express.json()])
 
@@ -57,3 +43,63 @@ app.use('/api', apiRoute)
 
 // Export lambda handler
 exports.handler = serverless(app)
+
+// /* eslint-disable no-undef */
+// // ? For absolute path enable import function instead of require
+// const fs = require('fs')
+// global.include = z => {
+//   let x = __dirname.replace(/\\/g, '/')
+//   let y = x.replace('/node_modules/node-absolute-path', '')
+//   let ext = ['', '.ts', '.tsx', '.js', '.jsx', '.html', '.css', '.png', '.jpg']
+//   for (let i = 0; i < ext.length; i++) {
+//     if (fs.existsSync(`${y}/${z}${ext[i]}`)) {
+//       z = `${y}/${z}`
+//     }
+//   }
+//   return require(z)
+// }
+
+// // ? For accessing .env file variables
+// require('dotenv').config()
+
+// const express = require('express'),
+//   db = require('mongoose'),
+//   serverless = require('serverless-http'),
+//   { apiRoute } = include('routes'),
+//   app = express()
+
+// db.connect(
+//   process.env.NODE_ENV === 'development'
+//     ? process.env.LOCAL_MONGO_URI
+//     : process.env.REMOTE_MONGO_URI
+// )
+
+// db.connection.on('connected', () => console.log(`DB connected successfully`))
+
+// db.connection.on('error', error => console.error(error.message))
+
+// // apiRoute.get('/', (req, res) =>
+// //   res.status(200).json({ message: 'Ping Successful API Server Running...' })
+// // )
+
+// // apiRoute.get('/users', (req, res) =>
+// //   res.json({
+// //     users: [
+// //       {
+// //         name: 'steve'
+// //       },
+// //       {
+// //         name: 'joe'
+// //       }
+// //     ]
+// //   })
+// // )
+
+// // apiRoute.get('/hello', (req, res) => res.json({ msg: 'hello world' }))
+
+// app.use([express.urlencoded({ extended: true }), express.json()])
+
+// app.use('/api', apiRoute)
+
+// // Export lambda handler
+// exports.handler = serverless(app)
